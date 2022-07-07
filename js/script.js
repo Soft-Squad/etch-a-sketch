905b2c18af84f569d0eac6799316ce7efc24164b
Alexander Winters <wintersalex@icloud.com>
2022-07-07 14:48:24 -0700

    Add event listeners for mouseover and mouseout

--- js/script.js
+++ js/script.js
@@ -8,8 +8,18 @@ function createDefaultGrid(rows, cols) {
     for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
         cell.innerText = (c + 1);
-        gridContainer.appendChild(cell).className = "grid-item";
+        gridContainer.appendChild(cell).className = "gridCell";
     }
 }
 
 createDefaultGrid(16, 16)     // Start
+
+let gridItem = document.getElementsByClassName("gridCell");
+for (const cell of gridItem) {
+    cell.addEventListener('mouseover', function(e) {
+        e.target.style.background = 'coral';
+    });
+    cell.addEventListener('mouseout', function(e) {
+        e.target.style.background = '';
+    })
+};
